8ef68a93185bc3e89aad3b4c3a8d0a6bea82b93c
Anna <annabrewer@berkeley.edu>
2021-10-10 08:08:51 -0700

    added Oski and Campanile, fixed hand shader

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,10 +6,11 @@ using UnityEngine.AI;
 public class Monster : MonoBehaviour {
 
     public enum State {
-        ALIVE, DYING, SINKING
+        ALIVE, DYING, SINKING, ATTACKING, VICTORY
     }
 
     public State monsterState = State.ALIVE;
+    public State previousMonsterState = State.ALIVE;
 
     public GameObject player;
     public float attackRange;
@@ -27,6 +28,11 @@ public class Monster : MonoBehaviour {
 
     public float sinkSpeed;
 
+    public GameObject walking;
+    public GameObject attack1;
+    public GameObject attack2;
+    public GameObject dancing;
+
 	// Use this for initialization
 	void Start () {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -34,11 +40,13 @@ public class Monster : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
         audioSource.PlayOneShot(spawnClip);
         currHealth = maxHealth;
+
+        SetActiveModel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (monsterState == State.ALIVE) {
+        if (monsterState == State.ALIVE || monsterState == State.ATTACKING) {
             navMeshAgent.SetDestination(player.transform.position);
 
             Vector3 distanceVector = transform.position - player.transform.position;
@@ -46,20 +54,36 @@ public class Monster : MonoBehaviour {
             float distance = distanceVector.magnitude;
 
             if (distance <= attackRange) {
-                animator.SetBool("Attack", true);
+                monsterState = State.ATTACKING;
+                navMeshAgent.isStopped = true;
+            } else {
+                navMeshAgent.isStopped = false;
             }
         } else if (monsterState == State.SINKING) {
             float sinkDistance = sinkSpeed * Time.deltaTime;
             transform.Translate(new Vector3(0, -sinkDistance, 0));
         }
+
+        if (monsterState != previousMonsterState)
+        {
+            SetActiveModel();
+            monsterState = previousMonsterState;
+        }
 	}
+
+    public void SetActiveModel() {
+        walking.SetActive(monsterState == State.ALIVE || monsterState == State.DYING);
+        attack1.SetActive(monsterState == State.ATTACKING);
+        attack2.SetActive(false);
+        dancing.SetActive(monsterState == State.VICTORY);
+    }
 
     public void Attack() {
         audioSource.PlayOneShot(hitClip);
     }
 
     public void Hurt(int damage) {
-        if (monsterState == State.ALIVE) {
+        if (monsterState == State.ALIVE || monsterState == State.ATTACKING) {
             animator.SetTrigger("Hurt");
             currHealth -= damage;
             if (currHealth <= 0)
@@ -71,7 +95,6 @@ public class Monster : MonoBehaviour {
         monsterState = State.DYING;
         audioSource.PlayOneShot(dieClip);
         navMeshAgent.isStopped = true;
-        animator.SetTrigger("Dead");
     }
 
     public void StartSinking() {
